e7d36d0c08d92d217f89813e9e781a7b4b5c2481
SwatKat1977 <paulmorriss1977@gmail.com>
2025-04-16 17:38:27 +0100

    update as part of rename

--- src/test.cpp
+++ src/test.cpp
@@ -55,7 +55,7 @@ class Vertex {
     float w_;
 };
 
-#include "wavefront/WaveFrontObjLoader.h"
+#include "wavefront/WaveFrontObjParser.h"
 #include "Meshborn.h"
 #include "Logger.h"
 
--- src/wavefront/WaveFrontObjParser.cpp
+++ src/wavefront/WaveFrontObjParser.cpp
@@ -21,7 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <utility>
 #include "glm/glm.hpp"
 #include "LoggerManager.h"
-#include "WaveFrontObjLoader.h"
+#include "WaveFrontObjParser.h"
 #include "MaterialLibraryParser.h"
 
 namespace Meshborn {
