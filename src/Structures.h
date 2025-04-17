2eafe276add77a473c109e5441821892c6a57800
SwatKat1977 <paulmorriss1977@gmail.com>
2025-04-17 02:03:40 +0100

    update header guard

--- src/Structures.h
+++ src/Structures.h
@@ -15,8 +15,8 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#ifndef ST_
-#define ST_
+#ifndef STRUCTURES_H_
+#define STRUCTURES_H_
 
 namespace Meshborn {
 
@@ -49,4 +49,4 @@ struct TextureCoordinates {
 
 }   // namespace Meshborn
 
-#endif  // ST_
+#endif  // STRUCTURES_H_
