3dc6ce3459351b497cb449afc5775edaf0d8289c
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-04-29 16:18:25 +0100

    Updated docstrings@

--- src/Model.h
+++ src/Model.h
@@ -25,14 +25,48 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 namespace Meshborn {
 namespace WaveFront {
 
+/**
+ * @class Model
+ * @brief Represents a 3D model composed of multiple meshes and materials.
+ *
+ * The Model class stores a collection of Mesh objects and associated
+ * materials.
+ * It tracks the total number of meshes and materials explicitly for
+ * convenience.
+ */
 class Model {
  public:
+    /**
+    * @brief Constructs an empty Model with zero meshes and materials.
+    */
     Model() : totalMeshes(0), totalMaterials(0) {}
 
+    /**
+     * @brief A list of meshes that make up the model.
+     */
     std::vector<Mesh> meshes;
+
+    /**
+     * @brief The total number of meshes in the model.
+     *
+     * This is typically equal to meshes.size(), but may be cached or used for
+     * external tracking.
+     */
     size_t totalMeshes;
 
+    /**
+     * @brief A mapping of material identifiers to material data.
+     *
+     * Used to look up materials associated with the meshes in the model.
+     */
     MaterialMap materials;
+
+    /**
+     * @brief The total number of materials used in the model.
+     *
+     * This is typically equal to materials.size(), but may be cached or used
+     * for external tracking.
+     */
     size_t totalMaterials;
 };
 
