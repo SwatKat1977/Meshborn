f17e858133f9e6a598f2f61703e4299f4d5c2492
SwatKat1977 <paulmorriss1977@gmail.com>
2025-05-02 09:05:54 +0100

    added Vector3D class

---
+++ src/Meshborn/Vector3D.h
@@ -0,0 +1,102 @@
+/*
+Meshborn
+Copyright (C) 2025 SwatKat1977
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef VECTOR3D_H_
+#define VECTOR3D_H_
+#include <cmath>
+#include <stdexcept>
+#include <ostream>
+
+namespace Meshborn {
+
+class Vector3D {
+ public:
+    float x, y, z;
+
+    // Constructors
+    Vector3D() : x(0.f), y(0.f), z(0.f) {}
+    Vector3D(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    // Magnitude (length) of the vector
+    float Length() const {
+        return std::sqrt(x * x + y * y + z * z);
+    }
+
+    // Normalize this vector in-place
+    void Normalize() {
+        float len = Length();
+        if (len == 0.f)
+            throw std::runtime_error("Cannot normalize zero-length vector");
+        x /= len;
+        y /= len;
+        z /= len;
+    }
+
+    // Return a normalized copy of this vector
+    Vector3D Normalized() const {
+        float len = Length();
+        if (len == 0.f)
+            throw std::runtime_error("Cannot normalize zero-length vector");
+        return { x / len, y / len, z / len };
+    }
+
+    // Dot product
+    float Dot(const Vector3D& other) const {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    // Cross product
+    Vector3D Cross(const Vector3D& other) const {
+        return {
+            y * other.z - z * other.y,
+            z * other.x - x * other.z,
+            x * other.y - y * other.x
+        };
+    }
+
+    // Operators
+    Vector3D operator+(const Vector3D& rhs) const {
+        return { x + rhs.x, y + rhs.y, z + rhs.z };
+    }
+
+    Vector3D operator-(const Vector3D& rhs) const {
+        return { x - rhs.x, y - rhs.y, z - rhs.z };
+    }
+
+    Vector3D operator*(float scalar) const {
+        return { x * scalar, y * scalar, z * scalar };
+    }
+
+    Vector3D operator/(float scalar) const {
+        if (scalar == 0.f)
+            throw std::runtime_error("Division by zero");
+        return { x / scalar, y / scalar, z / scalar };
+    }
+
+    bool operator==(const Vector3D& rhs) const {
+        return x == rhs.x && y == rhs.y && z == rhs.z;
+    }
+
+    // Optional: stream output
+    friend std::ostream& operator<<(std::ostream& os, const Vector3D& v) {
+        return os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+    }
+};
+
+}   // namespace Meshborn
+
+#endif  //  VECTOR3D_H_
